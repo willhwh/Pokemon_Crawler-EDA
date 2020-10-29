480792078627951cef4567e95eaa4d1cb4bbfe87
willhwh <52117569+willhwh@users.noreply.github.com>
2020-10-29 13:33:48 -0500

    api document

---
+++ app.py
@@ -0,0 +1,129 @@
+########################################
+# IMPORTS
+########################################
+
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import pandas as pd
+import Mars_scrape
+import os
+
+
+########################################
+# SETUP APPLICATION
+########################################
+
+app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0  # Prevent caching
+mongo = PyMongo(app, uri="mongodb://localhost:27017/Pokemon_DB")
+
+
+
+########################################
+# CONSTANTS
+########################################
+
+api_current_version = "v1.0"
+
+routes = {
+    "home": "/",
+    "api_versions": "/api",
+    "api_docs": "/api/<version>",
+
+
+
+    "api_docs_year": "/api/<version>/year",
+    "api_docs_city": "/api/<version>/city",
+    "api_docs_precinct": "/api/<version>/precinct",
+    "api_docs_neighborhood": "/api/<version>/neighborhood",
+    "api_year": "/api/<version>/year/<year>",
+    "api_city": "/api/<version>/city/<cityID>",
+    "api_precinct": "/api/<version>/precinct/<precinctID>",
+    "api_neighborhood": "/api/<version>/neighborhood/<neighborhoodID>"
+}
+
+templates = {
+    "home": "index.html",
+    "api_docs": "api_docs.html",
+    "api_versions": "api_versions.html"
+}
+
+
+version_infos=[
+    {
+        "name":"v1.0",
+        "url":"/api/v1.0",
+        "documentation":{
+            "/api/v1.0/type":"Gets a list of available pokemon types.",
+            "/api/v1.0/type/<type>":[
+                {
+                    "Pokemon_Name" : {
+                        "Type1":"The first type of this Pokemon",
+                        "Type2":"The second type of this Pokemon",
+                        'HP':'The initial HP points for this Pokemon',
+                        'ATK':'The initial ATK points for this Pokemon',
+                        'DEF':'The initial DEF points for this Pokemon',
+                        'SP_ATK':'The initial SP_ATK points for this Pokemon',
+                        'SP_DEF':'The initial SP_DEF points for this Pokemon',
+                        'SPD':'The initial SPD points for this Pokemon',
+                        'Damaged Normally by':'A list of type this Pokemon is damaged noramlly by.',
+                        'Weak to':'A list of type this Pokemon is weak to.',
+                        'Immue to':'A list of type this Pokemon is immue to.',
+                        'Resisant to':'A list of type this Pokemon is resistan to.'
+                    }
+                }
+            ]
+            "/api/v1.0/name":"Gets a list of available Pokemon names.",
+            "/api/v1.0/name/<name>":[
+                {
+                    "Pokemon_Name" : {
+                        "Type1":"The first type of this Pokemon",
+                        "Type2":"The second type of this Pokemon",
+                        'HP':'The initial HP points for this Pokemon',
+                        'ATK':'The initial ATK points for this Pokemon',
+                        'DEF':'The initial DEF points for this Pokemon',
+                        'SP_ATK':'The initial SP_ATK points for this Pokemon',
+                        'SP_DEF':'The initial SP_DEF points for this Pokemon',
+                        'SPD':'The initial SPD points for this Pokemon',
+                        'Damaged Normally by':'A list of type this Pokemon is damaged noramlly by.',
+                        'Weak to':'A list of type this Pokemon is weak to.',
+                        'Immue to':'A list of type this Pokemon is immue to.',
+                        'Resisant to':'A list of type this Pokemon is resistan to.'
+                    }
+                }
+            ]
+
+
+        }
+
+
+
+
+    }
+]
+
+
+########################################
+# ROUTES
+########################################
+
+@app.route("/")
+def home():
+    mars_data = mongo.db.mars.find_one()
+    mars_table=pd.read_html('Mars_facts.html')
+    return render_template("index.html",mars_data=mars_data,mars_table=mars_table)
+
+@app.route("/api")
+def api_page():
+
+
+@app.route('/scrape')
+def scrape_mars():
+    mars_data=Mars_scrape.scrape()
+    mongo.db.mars.update({}, mars_data, upsert=True)
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
