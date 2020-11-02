2afe69a151bcc1286bb8d1cef3ccf319c2b16602
willhwh <52117569+willhwh@users.noreply.github.com>
2020-11-02 01:30:40 -0600

    name api added

--- app.py
+++ app.py
@@ -57,14 +57,14 @@ version_infos=[
             "/api/name/<name>":[
                 {
                     "Pokemon_Name" : {
-                        "Type1":"The first type of this Pokemon",
-                        "Type2":"The second type of this Pokemon",
-                        'HP':'The initial HP points for this Pokemon',
-                        'ATK':'The initial ATK points for this Pokemon',
-                        'DEF':'The initial DEF points for this Pokemon',
-                        'SP_ATK':'The initial SP_ATK points for this Pokemon',
-                        'SP_DEF':'The initial SP_DEF points for this Pokemon',
-                        'SPD':'The initial SPD points for this Pokemon',
+                        "Type1":"The first type of this Pokemon.",
+                        "Type2":"The second type of this Pokemon.",
+                        'HP':'The initial HP points for this Pokemon.',
+                        'ATK':'The initial ATK points for this Pokemon.',
+                        'DEF':'The initial DEF points for this Pokemon.',
+                        'SP_ATK':'The initial SP_ATK points for this Pokemon.',
+                        'SP_DEF':'The initial SP_DEF points for this Pokemon.',
+                        'SPD':'The initial SPD points for this Pokemon.',
                         'Damaged Normally by':'A list of type this Pokemon is damaged noramlly by.',
                         'Weak to':'A list of type this Pokemon is weak to.',
                         'Immue to':'A list of type this Pokemon is immue to.',
@@ -116,14 +116,42 @@ def type():
     -------
     A list of pokemon type
     """
-    
+    #Retrieved type list from database.
     type_document=pokemon_collection.find_one({"Search_Id":'Type_List'})
+
+    #Create type list json
     type_list={}
     types=[]
     for i in type_document["Types"]:
         types.append(i[1])
     type_list.update({"Type_List":types})
+
     return jsonify(type_list)
+
+
+
+@app.route(routes["name"])
+def name():
+    """
+    The name list.
+
+    Returns
+    -------
+    A list of pokemon name
+    """
+    #Retrieved name list from database.
+    name_document=pokemon_collection.find({"Search_Id":'Pokemon_List'})
+
+    #Create name list json
+    name_list={}
+    names=[]
+    for pokemon in name_document:
+        if pokemon['Name'] not in names:
+            names.append(pokemon['Name'])
+    name_list.update({"Name_List":names})
+
+    return jsonify(name_list)
+
 
 
 ########################################
